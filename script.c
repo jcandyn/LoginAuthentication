f6b9a15c6119a91bbe38384f3a0192b4f6f5573b
Joscandy <jos.candy@gmail.com>
2022-12-01 18:19:44 -0500

    finished hashtable and login main method

--- script.c
+++ script.c
@@ -3,12 +3,14 @@
 #include <string.h>
 
 #include <stdio.h>
+#include "hashtable.h"
 
-// #include "hashtable.h"
+#include <stdio.h>
+#include <time.h>
 
 /**
  * @file script.c
- * @author your name (you@domain.com)
+ * @author Joscandy Nunez
  * @brief
  * @version 0.1
  * @date 2022-11-22
@@ -17,11 +19,12 @@
  *
  */
 
-// read lines from a file, one at a time
 int main()
 {
 
     // create a hash data structure
+    /* Choose initial capacity of 500 */
+    HashTable *ht = create_table(5000);
 
     char *filename = "LoginsAndPasswords.txt";
     FILE *fp;
@@ -29,12 +32,21 @@ int main()
     size_t len = 0;
     ssize_t read;
 
-    /* Choose initial capacity of 10 */
-    /* Specify the size of the keys and values you want to store once */
+    /*CREATE OUTPUT FILE*/
+    // write to file
+    int num;
+    FILE *fptr;
 
-    char *username;
-    char *password;
+    // use appropriate location if you are using MacOS or Linux
+    fptr = fopen("signIn.txt", "w");
 
+    if (fptr == NULL)
+    {
+        printf("Error!");
+        exit(1);
+    }
+
+    /* READ IN FILE */
     fp = fopen(filename, "r");
     if (fp == NULL)
         exit(0);
@@ -43,45 +55,60 @@ int main()
     {
         if ((couple[0] = strtok(line, ",")) != NULL)
         {
-            couple[1] = strtok(NULL, ",");
+            couple[1] = strtok(NULL, ", ");
         }
-        username = (char *)malloc(sizeof(char) * strlen(couple[0]));
-        username = couple[0];
-        printf("username:::: %s ", username);
 
-        password = (char *)malloc(sizeof(char) * strlen(couple[1]));
-        password = couple[1];
-        printf("password:::: %s ", password);
-
-        // Then learn how to split each line into tokens (fields) using ',' as the delimiter
-        //  get separated tokens by username and password
-        // printf("\n split 0: %s", couple[0]);
-        // printf("\n split 1: %s", couple[1]);
-        // printf("\n Retrieved line of length %zu:\n", read);
-        // printf("%s", line);
-
-        fclose(fp);
-
-        // if (line)
-        //     free(line);
-        // exit(1);
-
-        return 0;
+        ht_insert(ht, couple[0], couple[1]);
     }
+    print_hashtable(ht);
+
+    // take in login from user
+
+    char name[50];
+    char pass[50];
+    printf("\nLogin:");
+    scanf("%s", &name);
+    printf("\nPassword:");
+    scanf("%s", &pass);
+
+    printf(ht_search(ht, name));
+    int count = 0;
+    while ((!strncmp(ht_search(ht, name), pass, sizeof(ht_search(ht, name))) == 0))
+    {
+        if (count >= 2)
+        {
+            printf("\nExceeded 3 Attempts. Account is locked!");
+            exit(0);
+        }
+        printf("\nFailed login!");
+
+        time_t rawtime;
+        struct tm *timeinfo;
+
+        time(&rawtime);
+        timeinfo = localtime(&rawtime);
+        printf("Current local time and date: %s", asctime(timeinfo));
+
+        fprintf(fptr, "\n%s Failed logged in %s", name, asctime(timeinfo));
+
+        printf("\nPassword:");
+        scanf("%s", &pass);
+        count++;
+    }
+
+    time_t rawtime;
+    struct tm *timeinfo;
+
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    printf("Current local time and date: %s", asctime(timeinfo));
+
+    fprintf(fptr, "\n%s Succesfully logged in %s", name, asctime(timeinfo));
+    printf("\nSuccesful Login!");
+
+    fclose(fp);
+
+    fclose(fptr);
+
+    return 0;
 }
-/**
- * @brief
-
-
-
-
-
-
-
-Then learn how to populate and read from this data structure (and print to stdout.)
-
-Each one of these should have its own function.
-
-Each of these have many tutorials available on line. Give it a go and come back when you are stuck. Show your work, explain what you have tried, describe where you are stuck. Turn on all compiler warnings.
- *
- */
