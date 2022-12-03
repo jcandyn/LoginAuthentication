571c3f6c7b131d5040c13576084baab4060c559b
Joscandy <jos.candy@gmail.com>
2022-12-02 21:36:23 -0500

    fixed length problem for real

--- script.c
+++ script.c
@@ -8,6 +8,15 @@
 #include <stdio.h>
 #include <time.h>
 
+#include <unistd.h>
+#include <errno.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 /**
  * @file script.c
  * @author Joscandy Nunez
@@ -18,9 +27,41 @@
  * @copyright Copyright (c) 2022
  *
  */
-
+void check_host_name(int hostname)
+{ // This function returns host name for local computer
+    if (hostname == -1)
+    {
+        perror("gethostname");
+        exit(1);
+    }
+}
+void check_host_entry(struct hostent *hostentry)
+{ // find host info from host name
+    if (hostentry == NULL)
+    {
+        perror("gethostbyname");
+        exit(1);
+    }
+}
+void IP_formatter(char *IPbuffer)
+{ // convert IP string to dotted decimal format
+    if (NULL == IPbuffer)
+    {
+        perror("inet_ntoa");
+        exit(1);
+    }
+}
 int main()
 {
+    char host[256];
+    char *IP;
+    struct hostent *host_entry;
+    int hostname;
+    hostname = gethostname(host, sizeof(host)); // find the host name
+    check_host_name(hostname);
+    host_entry = gethostbyname(host); // find host information
+    check_host_entry(host_entry);
+    IP = inet_ntoa(*((struct in_addr *)host_entry->h_addr_list[0])); // Convert into IP string
 
     // create a hash data structure
     /* Choose initial capacity of 500 */
@@ -69,15 +110,35 @@ int main()
 
     printf("\nLogin:");
     scanf("%s", &name);
+
+    while (ht_search(ht, name) == NULL)
+    {
+        printf("\nusername is not in our records, try again:");
+        printf("\nLogin:");
+        scanf("%s", &name);
+    }
+
     printf("Password:");
     scanf("%s", &pass);
 
     int count = 0;
+    printf("length of actual %d", strlen(ht_search(ht, name)));
+    printf("length of entered %d", strlen(pass));
 
     // have user retry password attempt if the entered password exceeds the length of the actual password or if it is equal to in length but not the same value
-    while (!((strncmp(ht_search(ht, name), pass, strlen(ht_search(ht, name)) - 2) || (strlen(pass) != strlen(ht_search(ht, name)) - 2)) == 0))
+    while (!((strncmp(ht_search(ht, name), pass, strlen(ht_search(ht, name)) - 2) == 0)) || (strlen(pass) != (strlen(ht_search(ht, name)) - 2)))
     {
-
+        if (strlen(pass) == strlen(ht_search(ht, name)))
+        {
+            if (strncmp(ht_search(ht, name), pass, strlen(ht_search(ht, name))))
+            {
+                exit(1);
+            }
+            else
+            {
+                break;
+            }
+        }
         if (count >= 2)
         {
             printf("\nExceeded 3 Attempts. Account is locked!");
@@ -91,7 +152,7 @@ int main()
         time(&rawtime);
         timeinfo = localtime(&rawtime);
 
-        fprintf(fptr, "\n%s failed to log in timestamp: %s", name, asctime(timeinfo));
+        fprintf(fptr, "\nHost name: %s (IP: %s) going by user %s failed to log in timestamp: %s", host, IP, name, asctime(timeinfo));
 
         printf("\nPassword:");
         scanf("%s", &pass);
@@ -103,7 +164,7 @@ int main()
 
     time(&rawtime);
     timeinfo = localtime(&rawtime);
-    fprintf(fptr, "\n%s succesfully logged in, timestamp : %s", name, asctime(timeinfo));
+    fprintf(fptr, "\nHost name: %s (IP: %s) going by user %s succesfully logged in, timestamp : %s", host, IP, name, asctime(timeinfo));
     printf("\nSuccesfully Logged in!");
 
     fclose(fp);
